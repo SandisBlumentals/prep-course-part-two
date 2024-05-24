56ad2a6423ffe3bfb2309b4a2f4ab0c40ab602a8
SandisBlumentals <162323022+SandisBlumentals@users.noreply.github.com>
2024-05-24 20:23:46 +0300

    Uploading tasks

--- src/16-second-largest/index.ts
+++ src/16-second-largest/index.ts
@@ -3,7 +3,8 @@
  *
  * Array of numbers are passed in the function, your task is to find the second largest number.
  */
-
-function secondLargest(array: number[]) {}
+function secondLargest(array: number[]) {
+    return array.sort((a, b) => a - b)[array.length-2];
+}
 
 export { secondLargest };
